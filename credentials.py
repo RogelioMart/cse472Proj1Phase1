e37c8253c6b7ce39b62f716a91759b507b7bfb24
nschwei1 <56129778+nschwei1@users.noreply.github.com>
2020-09-25 00:42:42 -0700

    refactor: move credentials into a separate file
    Placing credentials in their own file makes them easier to find and update, and when updated they will show up more explicitly in the commit history.

---
+++ credentials.py
@@ -0,0 +1,11 @@
+# credentials.py
+# Variables that represent user credentials for site APIs
+# OAuth specs: https://tools.ietf.org/html/rfc5849
+
+# Twitter API credentials
+API_KEY = "wxlEOzKTqckeTZT6TFZAxynh8" # id that identifies API consumer
+API_KEY_SECRET = "DWozBRNZXGrtiWEDwgvBQzsAJFuGwiwOfxVA7aVXBGq80mKYxf" # password to authenticate API consumer id
+ACCESS_TOKEN =  "1304294097037987840-8I2YhFtByfIx4RwDQCe2ANcmPLUKmp" # id that identifies the test user assigned to our Twitter app
+ACCESS_TOKEN_SECRET = "vcr9ZQwucmpRX5vUxkPDnazpcQc3GQPXfYJ093Vrj4E8b"
+
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAOOdHwEAAAAAHsSW%2FEONGXLg1umXXaiaeRk7etU%3DINP4FX4GwuEdfbK8hooawnepadIaxMUx8JKXjBKjs7x9DdGP9H"
