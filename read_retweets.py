8415a2c9231bd61d556f132722d343acefb1065b
Rogelio <rmart143@asu.edi>
2020-09-26 00:58:11 -0700

    retweet scrape prototype

--- read_retweets.py
+++ read_retweets.py
@@ -12,72 +12,49 @@ auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-kojima_ID = 1278672884131483648
-
-retweets_list = api.retweets(kojima_ID)
-
-for retweet in retweets_list:
-	print(retweet.user.screen_name)
-	
-'''
-def retweetRecur(userID, cntr):
-	
-	if (cntr <= 5)
-		cntr = cntr + 1
-		
-		get a list of the user's tweets.
-		
-		iterate through the tweet list only 3 times
-		
-			make a list of all users who retweeted current tweet.
-			if(tweet has retweets)
-				tweet iterator countr ++
-				
-				write to a txt file the user's screen name and who currently retweeted them.
-				
-				retweetRecur(retweeter's ID, cntr)
-			else 
-				go to next tweet. (to pull this off you'll need another number that iterates through that dictionary)
-		
-			
-		
-	return(0)
-		
-'''
 
 def retweetRecur(uScreenName, cntr, fPointer):
 	
-	if(cntr <= 5)
+	if(cntr <= 5):
+	
 		cntr = cntr + 1
 		
 		threeCheck = 0
 		
-		tweetIter = 0
+		#gets uScreenName 10 most recent tweets
+		usersTweets = api.user_timeline(screen_name = uScreenName, count=10, include_rts = True, tweet_mode = 'extended')
 		
-		rtsIter = 0
-		
-		usersTweets = api.user_timeline(screen_name = uScreamName, count=10, include_rts = True, tweet_mode = 'extended')
-		
-		while((threeCheck < 3) && (tweetIter < 10)):
+		for tweetData in usersTweets:
 			
-			retweets_list = api.retweets(usersTweets[tweetIter].id)
+			print("ID: " + str(tweetData.id))
+			print(tweetData.created_at)
+			print(tweetData.full_text)
+			print("\n")
 			
-			if(len(retweets_list) >= 3):
-				threeCheck = threeCheck + 1
-				tweetIter = tweetIter + 1
+			
+			#gets the retweets from one of the tweets
+			retweets_list = api.retweets(tweetData.id)
+			
+			for rts in retweets_list:
+			
+				print(rts.user.screen_name)
+			'''
+				if(threeCheck > 2):#breaks out of the loop of 3 retweets hav already been found
+					break
 				
-				fPointer = open("edgeFile.txt", "a")
-				
-				f.write(uScreenName + " " + retweets_list[rtsIter] + "\n")
-				
-				fPointer.close()
-				
-				retweetRecur(, cntr, fPointer)
-				
-			else:
-				tweetIter = tweetIter + 1
-				
-		return(0)
+				if(len(retweets_list) >= 3):
+					threeCheck = threeCheck + 1
+					
+					fPointer = open("edgeFile.txt", "a")
+					
+					f.write(uScreenName + " " + rts.user.screen_name + "\n")
+					
+					fPointer.close()
+					
+					retweetRecur(rts.user.screen_name, cntr, fPointer)
+			'''
+					
+	return(0)
 				
 				
 				
@@ -86,6 +63,9 @@ def main():
 	
 	fPointer = open("edgeFile.txt", "x")
 	
-	retweetRecur(, 0, fPointer)
+	fPointer.close()
+	
+	retweetRecur("HIDEO_KOJIMA_EN", 0, fPointer)
 	
 main()
+
