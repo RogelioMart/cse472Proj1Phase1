68612f8c92d5db5cdccb088ee71e7a9e5e505995
Rogelio <rmart143@asu.edi>
2020-09-25 18:15:06 -0700

    Code I made to to try and crawl data.

--- read_retweets.py
+++ read_retweets.py
@@ -18,3 +18,74 @@ retweets_list = api.retweets(kojima_ID)
 
 for retweet in retweets_list:
 	print(retweet.user.screen_name)
+	
+'''
+def retweetRecur(userID, cntr):
+	
+	if (cntr <= 5)
+		cntr = cntr + 1
+		
+		get a list of the user's tweets.
+		
+		iterate through the tweet list only 3 times
+		
+			make a list of all users who retweeted current tweet.
+			if(tweet has retweets)
+				tweet iterator countr ++
+				
+				write to a txt file the user's screen name and who currently retweeted them.
+				
+				retweetRecur(retweeter's ID, cntr)
+			else 
+				go to next tweet. (to pull this off you'll need another number that iterates through that dictionary)
+		
+			
+		
+	return(0)
+		
+'''
+
+def retweetRecur(uScreenName, cntr, fPointer):
+	
+	if(cntr <= 5)
+		cntr = cntr + 1
+		
+		threeCheck = 0
+		
+		tweetIter = 0
+		
+		rtsIter = 0
+		
+		usersTweets = api.user_timeline(screen_name = uScreamName, count=10, include_rts = True, tweet_mode = 'extended')
+		
+		while((threeCheck < 3) && (tweetIter < 10)):
+			
+			retweets_list = api.retweets(usersTweets[tweetIter].id)
+			
+			if(len(retweets_list) >= 3):
+				threeCheck = threeCheck + 1
+				tweetIter = tweetIter + 1
+				
+				fPointer = open("edgeFile.txt", "a")
+				
+				f.write(uScreenName + " " + retweets_list[rtsIter] + "\n")
+				
+				fPointer.close()
+				
+				retweetRecur(, cntr, fPointer)
+				
+			else:
+				tweetIter = tweetIter + 1
+				
+		return(0)
+				
+				
+				
+				
+def main():
+	
+	fPointer = open("edgeFile.txt", "x")
+	
+	retweetRecur(, 0, fPointer)
+	
+main()
